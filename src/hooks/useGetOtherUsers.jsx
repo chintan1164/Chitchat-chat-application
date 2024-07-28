bc67dd650fe2df7db8525025541ad27339bb1f99
chintan1164 <127584756+chintan1164@users.noreply.github.com>
2024-07-28 21:17:51 +0530

    Update useGetOtherUsers.jsx

--- src/hooks/useGetOtherUsers.jsx
+++ src/hooks/useGetOtherUsers.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
 import { setOtherUsers } from '../redux/userSlice';
+import { BASE_URL } from '..';
 
 const useGetOtherUsers = () => {
 
@@ -11,7 +12,7 @@ const useGetOtherUsers = () => {
         const fetchOtherUsers = async () => {
             try {
                 axios.defaults.withCredentials = true;
-                const res = await axios.get(`http://localhost:8000/api/v1/user/`);
+                const res = await axios.get(`${BASE_URL}/api/v1/user/`);
                 dispatch(setOtherUsers(res.data))
             } catch (error) {
                 console.log(error)
