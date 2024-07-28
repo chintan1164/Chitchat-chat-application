9e97f6c71c410f43ffeb66dbb1cf927926829147
chintan1164 <127584756+chintan1164@users.noreply.github.com>
2024-07-28 23:09:59 +0530

    Update SendInput.jsx

--- frontend/src/components/SendInput.jsx
+++ frontend/src/components/SendInput.jsx
@@ -4,6 +4,7 @@ import { IoSend } from 'react-icons/io5';
 import { MdOutlineAddPhotoAlternate } from "react-icons/md";
 import { useDispatch, useSelector } from 'react-redux';
 import { setMessages } from '../redux/messageSlice';
+import { BASE_URL } from '..';
 
 const SendInput = () => {
   const [message, setMessage] = useState("");
@@ -18,7 +19,7 @@ const SendInput = () => {
       return;
     }
     try {
-      const res = await axios.post(`http://localhost:8000/api/v1/message/send/${selectedUser?._id}`, { message }, {
+      const res = await axios.post(`${BASE_URL}/api/v1/message/send/${selectedUser?._id}`, { message }, {
         headers: {
           'Content-Type': 'application/json'
         },
@@ -42,7 +43,7 @@ const SendInput = () => {
     formData.append('photo', file);
 
     try {
-      const res = await axios.post(`http://localhost:8000/api/v1/message/send-photo/${selectedUser?._id}`, formData, {
+      const res = await axios.post(`${BASE_URL}/api/v1/message/send-photo/${selectedUser?._id}`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         },
